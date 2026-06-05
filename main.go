c1b05c88c23feb0edb514665c9f1f34ffe9ee072
catimothe <timothe.camara@ynov.com>
2026-06-05 14:58:20 +0200

    feat: ajout de la page couleur

--- main.go
+++ main.go
@@ -13,5 +13,10 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", IndexHandler)
 	// to add : color functionality
+	http.HandleFunc("/color", ColorHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
 }
